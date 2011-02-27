2ae0c7b9477fcfd869675ce92c15979e09254e31
Aditya Sarawgi <sarawgi.aditya@gmail.com>
2011-02-27 19:48:52 +0530

    Add swank mode

--- init.el
+++ init.el
@@ -79,3 +79,21 @@
  (color-theme-molokai)
  ;(if window-system
  ;    color-theme-charcoal-black))
+
+;;
+;; swank-clojure
+(add-to-list 'load-path "~/opt/swank-clojure/src/emacs")
+
+(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
+      swank-clojure-extra-classpaths (list
+				            "~/opt/swank-clojure/src/main/clojure"
+					          "~/.clojure/clojure-contrib.jar"))
+
+(require 'swank-clojure-autoload)
+
+;; slime
+(eval-after-load "slime" 
+  '(progn (slime-setup '(slime-repl))))
+
+(require 'slime)
+(slime-setup) 
