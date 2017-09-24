5406c310b84bf876a899254b08dbb903fdc05562
Aditya Sarawgi <aditya@uber.com>
2017-09-23 17:04:58 -0700

    indent

--- init.el
+++ init.el
@@ -27,10 +27,10 @@
 (require 'package)
 (setq
    package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-		                          ("org" . "http://orgmode.org/elpa/")
-					                      ("melpa" . "http://melpa.org/packages/")
-							                          ("melpa-stable" . "http://stable.melpa.org/packages/"))
-    package-archive-priorities '(("melpa-stable" . 1)))
+                      ("org" . "http://orgmode.org/elpa/")
+                      ("melpa" . "http://melpa.org/packages/")
+                      ("melpa-stable" . "http://stable.melpa.org/packages/"))
+   package-archive-priorities '(("melpa-stable" . 1)))
 
 (package-initialize)
 (when (not package-archive-contents)
