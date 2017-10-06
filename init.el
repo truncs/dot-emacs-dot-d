559ef0c4de5b92bee214e71bb3a1913b8b051ded
Aditya Sarawgi <aditya@uber.com>
2017-10-05 23:36:33 -0700

    add a hl-todo package and add as a hook to prog modes

--- init.el
+++ init.el
@@ -175,6 +175,11 @@
 (use-package go-mode
   :defer t)
 
+(use-package hl-todo
+  :defer t
+  :config 
+  (add-hook 'prog-mode-hook 'hl-todo))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -182,7 +187,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit ac-dabbrev go-autocomplete auto-complete-config smex ace-jump-mode go-mode auto-complete ensime dracula-theme use-package evil))))
+    (hl-todo magit ac-dabbrev go-autocomplete auto-complete-config smex ace-jump-mode go-mode auto-complete ensime dracula-theme use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
