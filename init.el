7367f816a12fe9a451742ec9e5eeeb4dfe6361ea
Aditya <truncs.net>
2022-07-18 17:39:26 -0700

    Add sidebar mode, tabline mode

--- init.el
+++ init.el
@@ -182,26 +182,17 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
-
+ '(column-number-mode t)
  '(custom-enabled-themes '(xcode))
  '(custom-safe-themes
-   (quote
-    ("5ff3a60e4766a4b74afa7268c9a57c7a72ebb7386ae2629b4cee27562b1e9e90" "5a05a46286e47d1bda8151b37fea5e7e38be1e291f19fb73402a76fbef5ee95c" "4cd89e46d7fc2060e18021cf4a89f6af28661aba6a454e445cce2666b1611ab7" "1805fb283472cbe44571b8035125e3f575889fc9c4c0c6335622ac7c9938da5d" "aa5890424b1fec35d8bd1b2b7d3218bf3abc279f4a6f6172c498d3cfa1f81bf1" "14314fdae0c75ea8d2e28187213015a8cd881d0dad3e474b397158658aada668" "1598d2288563022f68d889643984294c1b50c986dee338ad9f2b9c18f15e4148" "078bde11b5fc0b7902a40262dda4db73be1b6c5158c8d76d4cddea98b77807e0" "d4bd862e3fd3985c00a5bd7fb32d574a119bd515ab49c7737690c26acfa3092f" "fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" "040c9c002d6b9af54c70186494f92609b150b24426c6f014b935ef504d964965" "60c9fed6a113923f49343520221c95628c203c7f8553e01843d136603328b823" "2a7beed4f24b15f77160118320123d699282cbf196e0089f113245d4b729ba5d" "617341f1be9e584692e4f01821716a0b6326baaec1749e15d88f6cc11c288ec6" default)))
+   '("5ff3a60e4766a4b74afa7268c9a57c7a72ebb7386ae2629b4cee27562b1e9e90" "5a05a46286e47d1bda8151b37fea5e7e38be1e291f19fb73402a76fbef5ee95c" "4cd89e46d7fc2060e18021cf4a89f6af28661aba6a454e445cce2666b1611ab7" "1805fb283472cbe44571b8035125e3f575889fc9c4c0c6335622ac7c9938da5d" "aa5890424b1fec35d8bd1b2b7d3218bf3abc279f4a6f6172c498d3cfa1f81bf1" "14314fdae0c75ea8d2e28187213015a8cd881d0dad3e474b397158658aada668" "1598d2288563022f68d889643984294c1b50c986dee338ad9f2b9c18f15e4148" "078bde11b5fc0b7902a40262dda4db73be1b6c5158c8d76d4cddea98b77807e0" "d4bd862e3fd3985c00a5bd7fb32d574a119bd515ab49c7737690c26acfa3092f" "fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" "040c9c002d6b9af54c70186494f92609b150b24426c6f014b935ef504d964965" "60c9fed6a113923f49343520221c95628c203c7f8553e01843d136603328b823" "2a7beed4f24b15f77160118320123d699282cbf196e0089f113245d4b729ba5d" "617341f1be9e584692e4f01821716a0b6326baaec1749e15d88f6cc11c288ec6" default))
  '(package-selected-packages
-   (quote
-    (jedi gnu-elpa-keyring-update ack docker-tramp rust-mode yaml-mode multi-term yasnippet-snippets markdown-mode interleave company-mode hl-todo magit ac-dabbrev go-autocomplete auto-complete-config smex ace-jump-mode go-mode auto-complete dracula-theme use-package evil)))
+   '(powerline dired-sidebar jedi gnu-elpa-keyring-update ack docker-tramp rust-mode yaml-mode multi-term yasnippet-snippets markdown-mode interleave company-mode hl-todo magit ac-dabbrev go-autocomplete auto-complete-config smex ace-jump-mode go-mode auto-complete dracula-theme use-package evil powerline dired-sidebar))
  '(tool-bar-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "SF Mono" :foundry "APPL" :slant normal :weight normal :height 88 :width normal)))))
-
 
 ;; Use xcode theme
 (load  (expand-file-name "themes/xcode-theme.el" user-emacs-directory))
+(load-theme 'xcode t)
 
 ;; modes
 (electric-indent-mode 0)
@@ -215,3 +206,55 @@
 (setq jedi:tooltip-method nil)
 (setq jedi:get-in-function-call-delay 0)
 
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+
+(require 'powerline)
+(global-tab-line-mode)
+(defvar my/tab-height 20)
+(defvar my/tab-left (powerline-wave-right 'tab-line nil my/tab-height))
+(defvar my/tab-right (powerline-wave-left nil 'tab-line my/tab-height))
+
+(defun my/tab-line-tab-name-buffer (buffer &optional _buffers)
+  (powerline-render (list my/tab-left
+                          (format " %s  " (buffer-name buffer))
+                          my/tab-right)))
+(setq tab-line-tab-name-function #'my/tab-line-tab-name-buffer)
+(setq tab-line-new-button-show nil)
+(setq tab-line-close-button-show nil)
+
+(set-face-attribute 'tab-line nil ;; background behind tabs
+      :background "gray40"
+      :foreground "gray60" :distant-foreground "gray50"
+      :family "Fira Sans Condensed" :height 1.0 :box nil)
+(set-face-attribute 'tab-line-tab nil ;; active tab in another window
+      :inherit 'tab-line
+      :foreground "gray70" :background "gray90" :box nil)
+(set-face-attribute 'tab-line-tab-current nil ;; active tab in current window
+      :background "#b34cb3" :foreground "white" :box nil)
+(set-face-attribute 'tab-line-tab-inactive nil ;; inactive tab
+      :background "gray80" :foreground "black" :box nil)
+(set-face-attribute 'tab-line-highlight nil ;; mouseover
+      :background "white" :foreground 'unspecified)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "SF Mono" :foundry "APPL" :slant normal :weight normal :height 88 :width normal)))))
